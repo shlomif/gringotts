745e036aeab17889d02ba357ad17ba5fa26fa88d
Shlomi Fish <shlomif@shlomifish.org>
2024-04-05 04:54:58 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- site/wml/gen-helpers.pl
+++ site/wml/gen-helpers.pl
@@ -1,23 +1,21 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 
-use HTML::Latemp::GenMakeHelpers;
+use HTML::Latemp::GenMakeHelpers ();
 
-my $generator =
-    HTML::Latemp::GenMakeHelpers->new(
-        'hosts' =>
-        [
-            map
-            {
-                +{ 'id' => $_, 'source_dir' => "src/$_",
-                   'dest_dir' => "\$(ALL_DEST_BASE)/$_",
-               },
-            }
-            (qw(common main))
-        ]
-    );
+my $generator = HTML::Latemp::GenMakeHelpers->new(
+    'hosts' => [
+        map {
+            +{
+                'id'         => $_,
+                'source_dir' => "src/$_",
+                'dest_dir'   => "\$(ALL_DEST_BASE)/$_",
+            },
+        } (qw(common main))
+    ]
+);
 
 $generator->process_all();
 
